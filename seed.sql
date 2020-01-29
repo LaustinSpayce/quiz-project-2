925b33313dde2f54f21f5567b4335a09178a4748
Stuart Myers <stuart.myers@gmail.com>
2020-01-29 15:23:49 +0800

    added data to seed.sql and tables.sql

---
+++ seed.sql
@@ -0,0 +1,49 @@
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('1 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('2 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('3 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('4 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('5 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('6 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('7 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('8 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('9 - A is the correct answer', 'A', 'B', 'C', 'D');
+
+INSERT INTO question
+(question, answer_1, answer_2, answer_3, answer_4)
+VALUES
+('10 - A is the correct answer', 'A', 'B', 'C', 'D');
---
+++ tables.sql
@@ -0,0 +1,40 @@
+CREATE TABLE IF NOT EXISTS player (
+  id SERIAL PRIMARY KEY,
+  name TEXT
+);
+
+CREATE TABLE IF NOT EXISTS game (
+  id SERIAL PRIMARY KEY,
+  name TEXT,
+  number_of_questions
+);
+
+CREATE TABLE IF NOT EXISTS question (
+  id SERIAL PRIMARY KEY,
+  question TEXT,
+  answer_1 TEXT,
+  answer_2 TEXT,
+  answer_3 TEXT,
+  answer_4 TEXT
+);
+
+CREATE TABLE IF NOT EXISTS game_questions (
+  id SERIAL PRIMARY KEY,
+  question_id INT,
+  game_id INT,
+  ordering INT
+);
+
+CREATE TABLE IF NOT EXISTS game_player_questions (
+  id SERIAL PRIMARY KEY,
+  player_id INT,
+  game_questions_id INT,
+  player_answer INT
+);
+
+CREATE TABLE IF NOT EXISTS game_player (
+  id SERIAL PRIMARY KEY,
+  game_id INT,
+  player_id INT,
+  player_score INT
+);
