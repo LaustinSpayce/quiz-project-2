cf39f1fbf623dc088bf4a3a29ac4f8cd81697f61
Stuart Myers <stuart.myers@gmail.com>
2020-01-29 15:04:43 +0800

    Initial Commit

---
+++ controllers/game.js
@@ -0,0 +1,25 @@
+module.exports = (db) => {
+
+  /**
+   * ===========================================
+   * Controller logic
+   * ===========================================
+   */
+
+  let indexControllerCallback = (request, response) => {
+      db.game.getAll((error, allgame) => {
+        response.render('game/index', { allgame });
+      });
+  };
+
+
+  /**
+   * ===========================================
+   * Export controller functions as a module
+   * ===========================================
+   */
+  return {
+    index: indexControllerCallback,
+  };
+
+}
---
+++ routes.js
@@ -0,0 +1,20 @@
+module.exports = (app, allModels) => {
+
+
+  /*
+   *  =========================================
+   *  =========================================
+   *  =========================================
+   *  =========================================
+   *    ALL ROUTES FOR GAME CONTROLLER
+   *  =========================================
+   *  =========================================
+   *  =========================================
+   */
+
+  // require the controller
+  const gameControllerCallbacks = require('./controllers/game')(allModels);
+
+  app.get('/game', gameControllerCallbacks.index);
+  //app.get('/games/:id', games.getgame);
+};
