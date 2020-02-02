e917a8e2a63d3becf27ff6e61852b024c0396b1e
Stuart Myers <stuart.myers@gmail.com>
2020-02-02 17:31:37 +0800

    fix bug

--- db.js
+++ db.js
@@ -16,7 +16,7 @@ const url = require('url')
 var configs
 
 if (process.env.DATABASE_URL) {
-  const params = url.url(process.env.DATABASE_URL)
+  const params = url.URL(process.env.DATABASE_URL)
   const auth = params.auth.split(':')
 
   configs = {
