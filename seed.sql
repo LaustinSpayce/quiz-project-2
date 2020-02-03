e861e42eda63d0667e3aac750aa63cd43fff9792
Stuart Myers <stuart.myers@gmail.com>
2020-02-03 13:34:43 +0800

    Update seed with proper questions

--- seed.sql
+++ seed.sql
@@ -1,52 +1,52 @@
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('1 - A is the correct answer', 'A', 'B', 'C', 'D');
+('Kracow is the former capital of which European country?', 'Poland', 'Czech Republic', 'Estonia', 'Germany');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('2 - A is the correct answer', 'A', 'B', 'C', 'D');
+('American singer Roger Miller is perhaps best known for which hit song and karaoke classic?', 'King of the Road', 'Bridge Over Troubled Water', 'Dancing Queen', 'YMCA');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('3 - A is the correct answer', 'A', 'B', 'C', 'D');
+('The British Tea bag was introduced by which company in the UK in 1953?', 'Tetley', 'PG', 'Yorkshire Tea', 'Pek Sin Choon Pte Ltd.');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('4 - A is the correct answer', 'A', 'B', 'C', 'D');
+('What is the name for the object that is placed either side of an aeroplane wheel to stop it rolling?', 'Chock', 'Chuck', 'Chunk', 'Chipmunk');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('5 - A is the correct answer', 'A', 'B', 'C', 'D');
+('Which dinosaur when translated from the latin means "swift robber"?', 'Velociraptor', 'Diplodocus', 'Tyrannosaur', 'Stegosaurus');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('6 - A is the correct answer', 'A', 'B', 'C', 'D');
+('Before a Cascading Style Sheet is useful, you need to include one into your HTML page. What HTML tag can you use to apply CSS rules to a document?', 'style', 'link', 'script', 'papaya');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('7 - A is the correct answer', 'A', 'B', 'C', 'D');
+('Which of these is the English computer scientist and mathematician who is considered to be the father of theoretical computer science and artificial intelligence?', 'Alan Turing', 'Tim Berners-Lee', 'Christopher Columbus', 'Dr. Who');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('8 - A is the correct answer', 'A', 'B', 'C', 'D');
+('What is the conversion of the base-2 number, 00000111, into base-10?', '7', '6', '8', '111');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('9 - A is the correct answer', 'A', 'B', 'C', 'D');
+('The term "GUI" stands for...', 'Graphical User Interface ', 'Gorgeous Unidentified Idiot ', 'Graphics Unused Input ', 'Graphing Ultimate Interface ');
 
 INSERT INTO question
 (question, answer_1, answer_2, answer_3, answer_4)
 VALUES
-('10 - A is the correct answer', 'A', 'B', 'C', 'D');
+('Reputed to have invented the programming language COBOL, Grace Hopper was extremely influential in its development and in promoting its widespread use. She is said, incorrectly, to have invented what common computing term?', 'Bug', 'Crash', 'Hack', 'Papaya');
 
 -- INSERT INTO player
 -- (name, token, game_id)
@@ -58,16 +58,16 @@ INSERT INTO game
 VALUES
 ('testGame', 10, 1, 'password', 'starting');
 
-INSERT INTO game_questions
-(question_id, game_id, ordering)
-VALUES
-(1, 1, 1),
-(1, 2, 2),
-(1, 3, 3),
-(1, 4, 4),
-(1, 5, 5),
-(1, 6, 6),
-(1, 7, 7),
-(1, 8, 8),
-(1, 9, 9),
-(1, 10, 10);
+-- INSERT INTO game_questions
+-- (question_id, game_id, ordering)
+-- VALUES
+-- (1, 1, 1),
+-- (1, 2, 2),
+-- (1, 3, 3),
+-- (1, 4, 4),
+-- (1, 5, 5),
+-- (1, 6, 6),
+-- (1, 7, 7),
+-- (1, 8, 8),
+-- (1, 9, 9),
+-- (1, 10, 10);
