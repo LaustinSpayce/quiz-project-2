0d320800a573a1ffd07078edbb86f340caa0deae
Stuart Myers <stuart.myers@gmail.com>
2020-02-04 17:29:26 +0800

    added footer

--- views/layouts/defaultlayout.jsx
+++ views/layouts/defaultlayout.jsx
@@ -16,6 +16,11 @@ class DefaultLayout extends React.Component {
             <div className='container mt-3'>
               {this.props.children}
             </div>
+            <footer className='mt-5 pt-5 pb-5 footer bg-dark'>
+              <div className='container'>
+                <h4 className='text-center'>Standing on the shoulders of giants</h4>
+              </div>
+            </footer>
             <script src='https://code.jquery.com/jquery-3.4.1.slim.min.js' integrity='sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n' crossOrigin='anonymous'></script>
             <script src='https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js' integrity='sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo' crossOrigin='anonymous'></script>
             <script src='https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js' integrity='sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6' crossOrigin='anonymous'></script>
